6567f8f33ee2ffecebaad1b23a028740e64cc989
Ricardo Borillo <borillo@gmail.com>
2018-10-08 22:22:33 +0200

    Borramos el setup para matchers

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,18 +1,3 @@
 import expect from "expect";
 
-expect.extend({
-  toShowEpisodesInOrder(page) {
-    const first = page.obtainFirstEpisode();
-    const last = page.obtainLastEpisode();
-    const numberOfFilms = page.numberOfFilms();
-
-    if (first === "Episode 1" && last === `Episode ${numberOfFilms}`) {
-      return { pass: true };
-    }
-
-    return {
-      message: () => `expected ${received} to be ordered`,
-      pass: false
-    };
-  }
-});
+expect.extend({});
