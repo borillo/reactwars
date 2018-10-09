af20cbe97a0c4c4082441a18951934628ccea222
Ricardo Borillo <borillo@gmail.com>
2018-10-09 19:45:24 +0200

    Limpiamos el spec de registro de pelis

--- acceptance-testing/cypress/integration/films/register.spec.js
+++ acceptance-testing/cypress/integration/films/register.spec.js
@@ -1,22 +1,14 @@
-import FilmsPageObject from "../../pageobjects/FilmsPageObject";
-
 describe("Non official Films", () => {
-  let page;
+  beforeEach(() => {});
 
-  beforeEach(() => {
-    page = new FilmsPageObject(cy);
-    page.open();
-  });
+  /*
+  {
+    title: "Rogue One: A Star Wars Story",
+    director: "Gareth Edwards",
+    year: 2016,
+    poster: "https://upload.wikimedia.org/wikipedia/en/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png"
+  }
+  */
 
-  it("should allow to register new films", () => {
-    page.registerFilm({
-      title: "Rogue One: A Star Wars Story",
-      director: "Gareth Edwards",
-      year: 2016,
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png"
-    });
-
-    page.films().contains("Rogue One: A Star Wars Story");
-  });
+  it("should allow to register new films", () => {});
 });
