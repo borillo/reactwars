2593a53f39f8c34983a4f6cc895e08b1a0b1b8f8
Ricardo Borillo <borillo@gmail.com>
2018-11-22 17:20:22 +0100

    Añadimos llamada de para clarificar

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -5,6 +5,8 @@ import { cleanup } from "react-testing-library";
 afterEach(cleanup);
 
 expect.extend({
+  // Custom matcher call: expect(page).toShowEpisodesInOrder();
+
   toShowEpisodesInOrder(page) {
     const first = page.obtainFirstEpisode();
     const last = page.obtainLastEpisode();
